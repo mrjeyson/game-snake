98d5ee1e376560ca8f9b92ed275290c693d921b6
Jasurbek <jasurbekkurganbayev@gmail.com>
2025-04-08 09:36:05 +0900

    ...

--- app/src/main/java/com/jsoft/snakegame/MainActivity.kt
+++ app/src/main/java/com/jsoft/snakegame/MainActivity.kt
@@ -72,7 +72,6 @@ class MainActivity : ComponentActivity() {
 
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val game = Game(lifecycleScope)
