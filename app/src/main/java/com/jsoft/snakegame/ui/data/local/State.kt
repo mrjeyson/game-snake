07db22b1d6d76ae2bb180f7d86bc5cd43001689f
Jasurbek <jasurbekkurganbayev@gmail.com>
2025-12-13 15:01:52 +0900

    ....

--- app/src/main/java/com/jsoft/snakegame/ui/data/local/State.kt
+++ app/src/main/java/com/jsoft/snakegame/ui/data/local/State.kt
@@ -2,6 +2,7 @@ package com.jsoft.snakegame.ui.data.local
 
 data class State(
 
+
     val food: Pair<Int, Int>,
     val snake: List<Pair<Int, Int>>
 )
--- app/src/main/java/com/jsoft/snakegame/ui/data/model/Game.kt
+++ app/src/main/java/com/jsoft/snakegame/ui/data/model/Game.kt
@@ -14,7 +14,6 @@ import java.util.Random
 
 class Game(private val scope: CoroutineScope) {
 
-
     private val mutex = Mutex()
     private val mutableState =
         MutableStateFlow(State(food = Pair(5, 5), snake = listOf(Pair(7, 7))))
