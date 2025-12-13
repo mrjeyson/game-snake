a2a9b28e54f5157683aeebc7daad7b042e163e6f
Jasurbek <jasurbekkurganbayev@gmail.com>
2025-12-13 15:10:25 +0900

    ....

--- app/src/main/java/com/jsoft/snakegame/MainActivity.kt
+++ app/src/main/java/com/jsoft/snakegame/MainActivity.kt
@@ -46,7 +46,6 @@ import kotlinx.coroutines.CoroutineScope
 class MainActivity : ComponentActivity() {
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val game = Game(lifecycleScope)
